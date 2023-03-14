866230be09a2490a7dbcd77b720ea9993977ba6d
Melvin Melendrez <melvin_sss@hotmail.com>
2023-03-14 16:45:33 -0700

    dependences install

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-     <h1>hey perrro cometela </h1>
+     <h1>hey perrro cometela jaja</h1>
     </>
   )
 }
